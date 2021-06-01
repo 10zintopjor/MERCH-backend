26dce5669de23896c576141450688a50c431467c
jungtop <tenzintopjor27@gmail.com>
2021-06-01 19:52:17 +0530

    initial commit

---
+++ projectback/controllers/paymentb.js
@@ -0,0 +1,46 @@
+const braintree = require("braintree");
+
+const gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "useYourMerchantId",
+  publicKey: "useYourPublicKey",
+  privateKey: "useYourPrivateKey"
+});
+
+
+exports.getToken = (req,res) =>{
+
+gateway.clientToken.generate({
+customerId: aCustomerId}, (err, response) => {
+    if(err){
+        res.status(500).json(err);
+    }else{
+        res.send(response);
+    }
+});
+
+
+};
+
+
+
+exports.processpayment = (req,res) =>{
+let nonceFromTheClient = req.body.paymentMethodNonce;
+let amountFromTheClient = req.body.amount;
+
+gateway.transaction.sale({
+  amount: amountFromTheClient,
+  paymentMethodNonce: nonceFromTheClient,
+
+  options: {
+    submitForSettlement: true
+  }
+}, (err, result) => {
+    if(err){
+        res.status(500).json(err)
+    }else{
+        res.json(result);
+    }
+});
+
+};
